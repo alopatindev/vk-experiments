f7e21d32e23ac4248aeb4b4e34d750718c2957e3
Alexander Lopatin <sbar.geek@gmail.com>
2013-02-19 00:22:30 +0400

    initial commit

---
+++ diggroup.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+use Data::Printer;
+use VKontakte::Standalone;
+
+my $APP_ID = '1973922';
+
+#my $GROUP_ID = 'fair_elections';
+#my $GROUP_ID = 'brendetc';
+#my $GROUP_ID = 'kk_bezymie';
+my $GROUP_ID = '935610';
+
+my $vk = new VKontakte::Standalone:: $APP_ID;
+my $auth_uri = $vk->auth_uri('groups,friends');
+print $auth_uri;
+
+print "\nauthorizing... ";
+
+#my $auth_uri = $vk->auth_uri("audio");
+#system(('chromium-anon', $auth_uri));
+#print "input redirect link:";
+#my $where = <STDIN>;
+my $where = 'https://oauth.vk.com/blank.html#access_token=6a60c95ebd6180e7f325bc0997278a8735a7cf52523b6f37826ed589d9e3ea4172f0205a8bef4f0447e7a&expires_in=86400&user_id=156993436';
+
+$vk->redirected($where);
+
+print "ok\n";
+
+my $page_max = 1000;
+my $offset = 0;
+my $results = $vk->api("groups.getMembers", {gid => $GROUP_ID, offset => $offset});
+my $count = $results->{count};
+my $members = ();
+push(@members, @{$results->{users}});
+
+while (scalar(@members) + 1 < $count) { # FIXME: check this
+#while ($count > 0) {
+    $offset += $page_max;
+    $results = $vk->api("groups.getMembers", {gid => $GROUP_ID, offset => $offset});
+    $count = $results->{count};
+    push(@members, @{$results->{users}});
+    #p $results->{'users'};
+    print "OFFSET=$offset $#members < $count "; print scalar(@members),"\n\n";
+}
+
+##p $members;
+
+print "SCALAR=";
+print scalar(@members),"\n\n";
+#print @{$members}[0];
+#print "\n";
+#print @{$members}[1];
+#print "\n";
+#print @{$members}[999];
+#print "\n";
+#print $members[1000];
+
+##for $uid (@{$results->{'users'}}) {
+##    print "$uid\n";
+##}
+
+# counting friends
+
+foreach $member0 (@members) {
+    foreach $member1 (@members) {
+        print "areFriends($member0,$member1)\n";
+        $result = $vk->api("friends.areFriends", {uids => "$member0,$member1"});
+        print "friends=", @{$result}[0]->{friend_status},"\n";# == 3, "\n";
+        #print @{$result}[1]->{0};
+    }
+    print "\n";
+}
+
+#my $member0 = '33588551';
+#my $member1 = '552756';
+#print "areFriends($member0,$member1)\n";
+#$result = $vk->api("friends.areFriends", {uids => "$member0,$member1"});
+#p $result;
+##print "friends=", @{$result}[0]->{'friend_status'},"\n"# == 3, "\n";
